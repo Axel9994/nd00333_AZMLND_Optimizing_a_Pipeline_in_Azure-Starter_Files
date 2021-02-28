efce17c4d0dbc07c9f850a40d15a882cd8e746d1
Axel Aleman <axelalemanyi@hotmail.com>
2021-02-28 15:06:08 -0500

    My Solution of Project 1

--- train.py
+++ train.py
@@ -16,11 +16,8 @@ from azureml.data.dataset_factory import TabularDatasetFactory
 
 ds = TabularDatasetFactory.from_delimited_files(path="https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv")
 
-x, y = clean_data(ds)
 
-# TODO: Split data into train and test sets.
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.3)
 
 run = Run.get_context()
 
@@ -51,7 +48,11 @@ def clean_data(data):
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
 
     return x_df, y_df
-    
+
+ 
+x, y = clean_data(ds)   
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.3)
 
 def main():
     # Add arguments to script
